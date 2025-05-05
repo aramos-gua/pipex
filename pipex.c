a4c152822d7633e9d5b4af2ef01157253201ce49
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-05-05 18:54:42 +0100

    Send str through pipes

--- pipex.c
+++ pipex.c
@@ -14,34 +14,49 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int	main(void)
 {
-	int	pid;
+	int		fd[2];
+	int		pid;
 
+	if (pipe(fd) == -1)
+		return (1);
 	pid = fork();
 	if (pid == -1)
-		return (1);
+		return (2);
 	if (pid == 0)
 	{
 		//child process
-		int		file;
-		int		file2;
-		int		err;
-		//char	*arr[] = {"ping", "-c 3", "google.com", NULL};
-		//char	*env[] = {NULL};
+		close(fd[0]);
+		char	str[200];
+		printf("Input string: ");
+		fgets(str, 200, stdin);
+		str[strlen(str) - 1] = '\0';
 
-		file = open("pingResults.txt", O_WRONLY | O_CREAT, 0777);
-		if (file == -1)
-			return (2);
-		printf("The fd to pingResults.txt is %d\n", file);
-		file2 = dup2(file, STDOUT_FILENO);
-		printf("The duplicated fd: %d\n", file2);
-		close(file);
+		int len = strlen(str) + 1;
+		if (write(fd[1], &len, sizeof(int)) == -1)
+			return (4);
 
-		err = execlp("ping", "ping", "-c 1", "google.com", NULL);
-		if (err == -1)
+		if (write(fd[1], str, sizeof(char) * len) == -1)
 			return (3);
+		close(fd[1]);
+	}
+	else
+	{
+		//parent process
+		close(fd[1]);
+		char	str_p[200];
+		int		len_p;
+
+		if (read(fd[0], &len_p, sizeof(int)) == -1)
+			return (5);
+		if (read(fd[0], str_p, sizeof(char) * len_p) == -1)
+			return (6);
+		printf("Received %s \n", str_p);
+		close(fd[0]);
+		wait(NULL);
 	}
 	return (0);
 }
