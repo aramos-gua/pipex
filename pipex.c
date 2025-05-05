5982632500bece7df0bdf9eb364ba0c3a16a2299
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-05-05 16:42:41 +0100

    ping command

--- pipex.c
+++ pipex.c
@@ -15,31 +15,12 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-void	child_routine(pid_t pid, int *number)
-{
-	printf("Child routine started, pid is %d, number is: %d\n", pid, *number);
-}
-
-void	parent_routine(pid_t pid, int *number)
-{
-	printf("parent routine started\n");
-	*number = 24;
-	printf("PID is %d, number is %d\n", pid, *number);
-}
-
 int	main(void)
 {
-	pid_t	pid;
-	int		number;
+	char	*values[] = {"ping", "-c 3","google.com", NULL};
+	char	*env[] = {NULL};
 
-	number = 42;
-	printf("Before forking, the number is %d\n", number);
-	pid = fork();
-	if (pid == -1)
-		return (1);
-	else if (pid == 0)
-		child_routine(pid, &number);
-	else if (pid > 0)
-		parent_routine(pid, &number);
+	execve("/bin/ping", values, env);
+	printf("Success\n");
 	return (0);
 }
