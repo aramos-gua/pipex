9913ac073af2b64d4d59bfe1263b64dc19b8cbeb
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-05-10 16:38:10 +0100

    Working now! with more than 2 commands! finally! lets test this babyyyy

--- pipex.c
+++ pipex.c
@@ -50,6 +50,7 @@ char	*get_command_path(char *cmd, char **env)
 	char	*path_env = NULL;
 	char	*full_path;
 	int		i;
+	int		full_path_len;
 
 	i = 0;
 	while (env[i])
@@ -59,6 +60,7 @@ char	*get_command_path(char *cmd, char **env)
 			path_env = env[i] + 5;
 			break ;
 		}
+		i++;
 	}
 	if (!path_env)
 		return (NULL);
@@ -68,18 +70,23 @@ char	*get_command_path(char *cmd, char **env)
 	i = 0;
 	while (paths[i])
 	{
-		full_path = malloc(ft_strlen(paths[i]) + ft_strlen(cmd) + 2);
+		full_path_len = ft_strlen(paths[i]) + ft_strlen(cmd) + 2;
+		full_path = malloc(full_path_len);
 		if (!full_path)
+		{
+			i++;
 			continue ;
+		}
 		ft_strlcpy(full_path, paths[i], ft_strlen(paths[i]) + 1);
-		ft_strlcat(full_path, "/", ft_strlen(full_path) + 2);
-		ft_strlcat(full_path, cmd, ft_strlen(cmd) + 2);
+		ft_strlcat(full_path, "/", full_path_len);
+		ft_strlcat(full_path, cmd, full_path_len);
 		if (access(full_path, X_OK) == 0)
 		{
 			free_split(paths);
 			return (full_path);
 		}
 		free(full_path);
+		i++;
 	}
 	free_split(paths);
 	return (NULL);
@@ -119,7 +126,7 @@ void	free_split(char **arr)
 	if (!arr)
 		return ;
 	while (arr[i])
-		free(arr[i]);
+		free(arr[i++]);
 	free(arr);
 }
 
@@ -129,7 +136,7 @@ int	main(int argc, char **argv, char **envp)
 	pid_t	pid;
 	t_pipex	pipex;
 	int		i;
-	int		j;
+	int		k;
 
 	i = 0;
 	if (argc < 5)
@@ -137,7 +144,6 @@ int	main(int argc, char **argv, char **envp)
 	pipex.infile = open(argv[1], O_RDONLY); 
 	pipex.outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	pipex.cmd_count = argc - 3;
-	pipex.prev_fd = -1;
 	pipex.env = envp;
 	if (pipex.infile < 0 || pipex.outfile < 0)
 		return (ft_printf("File Error\n"), 1);
@@ -154,11 +160,17 @@ int	main(int argc, char **argv, char **envp)
 			close(pipes[i - 1][0]);
 		if (i < pipex.cmd_count - 1)
 			close(pipes[i][1]);
-		if (pipex.prev_fd != -1)
-			close(pipex.prev_fd);
+		i++;
 	}
+	k = 0;
 	close(pipex.infile);
 	close(pipex.outfile);
+	while (k < pipex.cmd_count - 1)
+	{
+		close(pipes[k][0]);
+		close(pipes[k][1]);
+		k++;
+	}
 	while (wait(NULL) > 0)
 		;
 	return (0);
